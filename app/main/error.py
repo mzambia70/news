05062cff9bea08e87c60c06b7a9696e4179706b2
mzambia70 <mzambia70@gmail.com>
2019-05-20 11:59:33 +0300

    imported the blueprint main

---
+++ app/main/error.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from . import main #importing the blueprint instance 'main' and use 2 define decorators
+
+@main.app.errorhandler(404)
+def four_Ow_four(error):
+    '''
+    Function to render the 404 error page
+    '''
+    return render_template('fourowfour.html'),404
